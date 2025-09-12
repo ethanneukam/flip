ded1a5aca92504fe086cf605dca6ca94a69aa586
ethanneukam <ethanneukam@gmail.com>
2025-09-12 15:58:38 -0400

    fix checkout

--- pages/api/checkout.ts
+++ pages/api/checkout.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-08-16", // stable version
+  apiVersion: "2025-08-27", // stable version
 });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
