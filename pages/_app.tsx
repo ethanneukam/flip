15a91509bc97fa438d0f1a194e6487c035924887
ethanneukam <ethanneukam@gmail.com>
2025-11-26 15:19:17 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,35 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import posthog from "posthog-js";
+
 import Header from "../components/Header";
 import AuthWrapper from "../components/AuthWrapper";
+import { Toaster } from "../components/ui/sonner"; // if you use a toast lib
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
+  // ---- PostHog Initialization ----
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      capture_pageview: true,
+    });
+  }, []);
+
   return (
-    <AuthWrapper>
-      <Header /> {/* Show header + notification bell on every page */}
-      <Component {...pageProps} />
-    </AuthWrapper>
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <AuthWrapper>
+        <Header />
+        <Component {...pageProps} />
+        <Toaster />
+      </AuthWrapper>
+    </SessionContextProvider>
   );
 }
