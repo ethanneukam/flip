09a96e6735ab548e69bb441de5974cc77379d2e0
ethanneukam <ethanneukam@gmail.com>
2025-11-27 22:55:25 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,3 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 }
 module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  productionBrowserSourceMaps: true,
+};
+
+module.exports = nextConfig;
