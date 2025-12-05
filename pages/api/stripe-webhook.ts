181507d90adbd166336fb1885cd17f1b6bf9d5cb
ethanneukam <ethanneukam@gmail.com>
2025-12-05 16:03:59 -0500

    Update stripe-webhook.ts

--- pages/api/stripe-webhook.ts
+++ pages/api/stripe-webhook.ts
@@ -10,7 +10,7 @@ export const config = {
 };
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-08-27.basil",
+ apiVersion: "2025-10-29.clover",
 });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
