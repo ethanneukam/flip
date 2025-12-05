3403dc6bb0fe0b618c9772833044b0c1b62319b0
ethanneukam <ethanneukam@gmail.com>
2025-12-05 18:28:10 -0500

    Update join-waitlist.ts

--- pages/api/join-waitlist.ts
+++ pages/api/join-waitlist.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { supabase } from "@/lib/supabase";
+import { supabase } from "@/lib/supabaseClient";
 import { nanoid } from "nanoid";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
