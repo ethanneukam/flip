333aa14d1d6fba0f804c2d31defa48d9b0130f00
ethanneukam <ethanneukam@gmail.com>
2025-12-09 16:47:56 -0500

    Update types.ts

--- scrapers/types.ts
+++ scrapers/types.ts
@@ -1,8 +1,13 @@
 export interface Scraper {
   source: string;
-  scrape: (page: any, keyword: string) => Promise<{
+  scrape: (
+    page: any,
+    keyword: string
+  ) => Promise<{
     price: number;
     url: string;
     condition: string;
+    shipping?: number | null;
+    seller_rating?: number | null;
   } | null>;
 }
