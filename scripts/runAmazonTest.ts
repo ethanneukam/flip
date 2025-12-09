7fd1b7f9bb3abca93837574e846f7589bff5f7e0
ethanneukam <ethanneukam@gmail.com>
2025-12-09 14:08:45 -0500

    Update runAmazonTest.ts

--- scripts/runAmazonTest.ts
+++ scripts/runAmazonTest.ts
@@ -6,7 +6,7 @@ async function run() {
   const browser = await chromium.launch({ headless: true });
   const page = await browser.newPage();
 
-  const result = await amazonScraper.scrape(page, "Nintendo Switch");
+  const result = await amazonScraper.run(page, "Nintendo Switch");
   console.log(result);
 
   await browser.close();
