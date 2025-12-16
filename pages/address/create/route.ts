5c01a901e41629a1a51f9188eccd8bd029dc0f4d
ethanneukam <ethanneukam@gmail.com>
2025-12-16 16:24:36 -0500

    Delete pages/address directory

--- pages/address/create/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST(req: Request) {
-  const data = await req.json();
-
-  // await prisma.shippingAddress.create({ data });
-
-  return NextResponse.json({ success: true });
-}
