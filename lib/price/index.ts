f0c5f2c27c81eee36ef1a78d8e34a11b8c3cb290
ethanneukam <ethanneukam@gmail.com>
2025-12-20 16:44:48 -0500

    Update index.ts

--- lib/price/index.ts
+++ lib/price/index.ts
@@ -1,3 +1,4 @@
 export { readPriceHistory as getPriceHistory } from "./readPrices";
+export { readExternalPrices as getExternalPrices } from "./readPrices";
 export * from "./readPrices";
 export * from "./writePrice";
