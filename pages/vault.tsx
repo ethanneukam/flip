35603ec626cfb450b3359323ae464b6027f88254
ethanneukam <ethanneukam@gmail.com>
2025-12-28 10:24:25 -0500

    Update vault.tsx

--- pages/vault.tsx
+++ pages/vault.tsx
@@ -1,7 +1,7 @@
 // pages/vault.tsx
 import { useEffect, useState } from 'react';
 import Head from 'next/head';
-import BottomNav from '../components/layout/BottomNav';
+import BottomNav from '../components/BottomNav';
 import NetWorthCard from '../components/vault/NetWorthCard';
 import { VaultService } from '../lib/vault-service';
 import { calculatePortfolio } from '../lib/valuation';
