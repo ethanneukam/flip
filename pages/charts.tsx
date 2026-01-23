cdb0532a4b608b9a1bd2cc115569d96b9afd91fa
ethanneukam <ethanneukam@gmail.com>
2026-01-23 14:47:37 -0500

    Update charts.tsx

--- pages/charts.tsx
+++ pages/charts.tsx
@@ -330,6 +330,12 @@ return (
       .custom-scrollbar::-webkit-scrollbar-track { background: transparent; }
       .custom-scrollbar::-webkit-scrollbar-thumb { background: rgba(255,255,255,0.1); border-radius: 10px; }
     `}</style>
+    {isAlertModalOpen && data && (
+  <PriceAlertModal 
+    item={{ ...data, ticker }} 
+    onClose={() => setIsAlertModalOpen(false)} 
+  />
+)}
   </main>
 );
 }
