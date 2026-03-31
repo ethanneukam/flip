a7b9e7210b17bc2ce4a7ce1857d66b8c198ccb1c
ethanneukam <ethanneukam@gmail.com>
2026-03-31 16:43:20 +0000

    hyh

--- layout.tsx
+++ layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import InstallPrompt from "@/components/InstallPrompt";
+import '../styles/globals.css'
 
 const inter = Inter({ subsets: ["latin"] });
 
