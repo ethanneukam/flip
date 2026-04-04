178377bdc0fb2497a07ff1f328f2cc0eb2a2e2d4
ethanneukam <ethanneukam@gmail.com>
2026-04-04 17:08:35 +0000

    wewew

--- flip-mobile/lib/supabase.ts
+++ flip-mobile/lib/supabase.ts
@@ -1,12 +1,14 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import { Platform } from 'react-native';
 
 const supabaseUrl = 'https://eugjprzdksgattiigvnk.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1Z2pwcnpka3NnYXR0aWlndm5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5MjQxMDIsImV4cCI6MjA3MDUwMDEwMn0.X72FMF_h_9cm1G1uGmMwxsq4XpPx3MpOHCiaGhxZ4LU';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: AsyncStorage,
+    // Only use AsyncStorage if we are NOT in a web-ssr environment
+    storage: Platform.OS !== 'web' ? AsyncStorage : undefined,
     autoRefreshToken: true,
     persistSession: true,
   },
